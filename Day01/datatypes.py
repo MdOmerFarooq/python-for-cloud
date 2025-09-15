8d2bb82c5a583c0087868ac2601c7010e947d930
MdOmerFarooq <farooqomarfarooqomar@gmail.com>
2025-09-15 09:52:15 +0530

    pyhton_Day01

---
+++ Day01/datatypes.py
@@ -0,0 +1,48 @@
+# A data type is a classification or categorization that specifies which type of data is stored in variable 
+
+# int --> integer values
+a = 2
+b = 3
+print(a+b)
+
+# str --> strings
+
+c = 'Hi i am a '
+d = 'Cloud engineer'
+print (c+d)
+
+# we can use + between two str to concatinate(add) them 
+
+## Task : we can also use * between str and int to multiply the str the times of int number 
+
+print(a*d)  # o/p --> Cloud engineerCloud engineer 
+
+# Float --> numbers with decimal points. Example: 3.14
+e = 1.5
+d = 2.4
+f = e+d #will add the value of e and d 
+print(f) # prints the value f 
+
+# complex: Represents complex numbers. 
+z = 2 + 3j
+
+# list: Represents lists (ordered, mutable sequences). 
+my_list = [1, 2, 3]
+print(my_list)
+
+# tuple: Represents tuples (ordered, immutable sequences).
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# set: Represents sets (unordered collections of unique elements).
+my_set = {1, 2, 3}
+print(my_set)
+
+# frozenset: Represents immutable sets. Example: 
+my_frozenset = frozenset([1, 2, 3])
+print(my_frozenset)
+
+
+
+
+
