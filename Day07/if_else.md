33f68d25ea0357712322a1a5a60edda5691eef95
MdOmerFarooq <farooqomarfarooqomar@gmail.com>
2025-09-20 11:03:54 +0530

    operators and conditional statements

---
+++ Day07/if_else.md
@@ -0,0 +1,4 @@
+# Conditional statements
+
+Conditional statements are a fundamental part of programming that allow us to make decisions and execute different blocks of code based on certain conditions. In Python, we can use if, elif ("else if"), and else to create conditional statements.
+
---
+++ Day07/if_else.py
@@ -0,0 +1,12 @@
+# practicing if else 
+
+num1 = 5
+num2 = 10
+
+if num1+num2==10 :  # 5+10=15 , 15 != 10 so this will return false
+    print("yes")    # since condition is false this will not get executed
+elif num1+num2==15 :   # 5+10=15 , 15 == 15 so this will return True
+    print("yes yes")  # since condition is True this will get executed
+else :               # since the above conditionn is true this will not get executed .
+    print("try again")
+
